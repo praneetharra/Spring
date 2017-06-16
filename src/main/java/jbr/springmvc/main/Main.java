86e9085ea0aa0d389adecd47cd9d0ac3030420b7
Praneeth Arra <parra@mtu.edu>
2017-06-16 00:49:01 -0700

    With executable jar

---
+++ src/main/java/jbr/springmvc/main/Main.java
@@ -0,0 +1,10 @@
+package jbr.springmvc.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- target/SpringMvcUser/WEB-INF/classes/jbr/springmvc/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package jbr.springmvc.service;
-
-public interface UserService {
-
-}
